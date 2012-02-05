85239e1f2a85b55f9b8c94b9fbb465adf2204af0
Christopher Turtle <hosmerangel@gmail.com>
2012-02-05 18:42:38 +0000

    get quizzes functionality, basic

---
+++ app/assets/javascripts/backbone/routers/appRouter.js
@@ -0,0 +1,16 @@
+kwisatz.routers.AppRouter = Backbone.Router.extend({
+    routes: {
+        ""          : "list",
+        "quiz/:id" : "quizDetails"
+    },
+ 
+    list: function() {
+        console.log('list');
+        this.quizList = new kwisatz.models.QuizSet();
+        this.quizListView = new kwisatz.views.QuizListView({model: this.quizList});
+        this.quizList.fetch();
+    },
+ 
+    quizDetails: function(id) {
+    }
+});
